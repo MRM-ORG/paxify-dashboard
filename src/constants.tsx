b97ded98bfb44e944267ed497f5b413a0f1a536c
Raffay Sajjad <raffaysajjad.apple@gmail.com>
2023-08-21 01:08:00 +0500

    Update BE URL

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,2 +1,2 @@
-// export const BACKEND_URL = "https://subscriptions.paxify.io";
-export const BACKEND_URL = "http://localhost:8000";
+export const BACKEND_URL = "https://subscriptions.paxify.io";
+// export const BACKEND_URL = "http://localhost:8000";
